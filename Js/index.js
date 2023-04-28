7f253803b200b7ee7d5fab6698f62be0b2f5e8f3
AmirHA <hasanabadi.am1380@gmail.com>
2023-04-28 12:33:12 -0700

    3 - main menu completed

---
+++ Js/index.js
@@ -0,0 +1,23 @@
+let menuBarBTN = document.getElementById('menuBarBTN');
+let mobileMenu = document.getElementById('mobileMenu');
+let menuBarBtnIcon = document.getElementById('menuBarBtnIcon');
+window.addEventListener('resize', function () {
+    if (window.innerWidth >= 768) {
+        mobileMenu.classList.add('d-none');
+        menuBarBtnIcon.classList.add('fas', 'fa-bars');
+        menuBarBtnIcon.classList.remove('fa', 'fa-close');
+    }
+})
+console.log(menuBarBtnIcon.classList);
+menuBarBtnIcon.addEventListener('click', function () {
+    if (menuBarBtnIcon.classList[1] == 'fa-bars' || menuBarBtnIcon.classList[3] == 'fa-bars') {
+        menuBarBtnIcon.classList.remove('fas', 'fa-bars');
+        menuBarBtnIcon.classList.add('fa', 'fa-close');
+        mobileMenu.classList.remove('d-none');
+    } else {
+        menuBarBtnIcon.classList.add('fas', 'fa-bars');
+        menuBarBtnIcon.classList.remove('fa', 'fa-close');
+        mobileMenu.classList.add('d-none');
+    }
+    console.log(menuBarBtnIcon.classList);
+})
